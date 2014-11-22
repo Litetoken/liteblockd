9429a7c0485fbb363b25fd3086bbd434fba8c044
Treefunder Incorporated <ceo@treefunder.com>
2014-11-22 00:53:09 -0500

    Czarcraft Factoring

--- lib/config.py
+++ lib/config.py
@@ -14,8 +14,8 @@ MARKET_PRICE_DERIVE_NUM_POINTS = 8 #number of last trades over which to derive t
 
 # FROM czarcraftd
 # NOTE: These constants must match those in czarcraftd/lib/config.py
-REGULAR_DUST_SIZE = 5430
-MULTISIG_DUST_SIZE = 5430 * 2
+REGULAR_DUST_SIZE = 5011
+MULTISIG_DUST_SIZE = 6011
 ORDER_LTC_DUST_LIMIT_CUTOFF = MULTISIG_DUST_SIZE
 
 mongo_db = None #will be set on server init
